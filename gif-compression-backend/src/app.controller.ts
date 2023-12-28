e3e1c3514b44d8ca347f14670abc066e5570a717
Joyee <veraqxf@gmail.com>
2023-12-28 17:56:12 +0800

    feat: 使用sharp压缩图片-后端代码

---
+++ gif-compression-backend/src/app.controller.ts
@@ -0,0 +1,64 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  ParseIntPipe,
+  Post,
+  Query,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { existsSync } from 'fs';
+import * as sharp from 'sharp';
+import { Response } from 'express';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      dest: 'uploads',
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    return file.path;
+  }
+
+  @Get('compression')
+  async compression(
+    @Query('path') filePath: string,
+    @Query('color', ParseIntPipe) color: number,
+    @Query('level') level: number,
+    @Res() res: Response,
+  ) {
+    console.log(filePath);
+    if (!existsSync(filePath)) {
+      throw new BadRequestException('文件不存在');
+    }
+
+    const data = await sharp(filePath, {
+      animated: true,
+      limitInputPixels: false,
+    })
+      .gif({
+        colors: color,
+      })
+      .toBuffer();
+
+    res.set('Content-Disposition', 'attachment; filename="dest.gif"');
+    res.send(data);
+
+    return 'success';
+  }
+}
